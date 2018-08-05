a0019f2122ee00dcbd62cc1cb59f34c003da5edf
Jonas Cheng <jonascheng@users.noreply.github.com>
2018-08-05 16:27:14 +0800

    issue my own token

--- contracts/erc20_tutorial.sol
+++ contracts/erc20_tutorial.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.18;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'JMT' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
-// Total supply: 100000000
+// Deployed to : 0x01e439aBBDf27863336a84EDD111A3396EAf142b
+// Symbol      : JMT
+// Name        : JM Token
+// Total supply: 100000000000000000000
 // Decimals    : 18
 //
 // Enjoy.
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract JMToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -112,13 +112,13 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    function FucksToken() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+    function JMToken() public {
+        symbol = "JMT";
+        name = "JM Token";
         decimals = 18;
-        _totalSupply = 100000000000000000000000000;
-        balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        _totalSupply = 100000000000000000000;
+        balances[0x01e439aBBDf27863336a84EDD111A3396EAf142b] = _totalSupply;
+        Transfer(address(0), 0x01e439aBBDf27863336a84EDD111A3396EAf142b, _totalSupply);
     }
 
 
